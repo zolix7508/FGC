4e396bd1ccf11f5420d4dcda0662d3464e3ac20d
zolix <zoli_x@yahoo.com>
2015-01-04 04:08:04 +0200

    x

---
+++ Games/mv/mvserver/mvResponse.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvserver
+{
+    [Serializable]
+    internal class mvResponse
+    {
+        private List<mvResponseItemBase> _items = new List<mvResponseItemBase>();
+
+        public void Add(mvResponseItemBase a)
+        {
+            _items.Add(a);
+        }
+
+        public void AddMsgItem(params object[] prms)
+        {
+            var item = new mvResponseMsgItem(prms);
+            _items.Add(item);
+        }
+
+        public void AddActionItem(ActionKind actionKind, params object[] prms)
+        {
+            var item = new mvResponseActionItem(actionKind, prms);
+            _items.Add(item);
+        }
+
+        public bool StatusChanged
+        {
+            get
+            {
+                return _items.OfType<mvResponseActionItem>().Any();
+            }
+        }
+
+        public IEnumerable<mvResponseItemBase> GetItems()
+        {
+            foreach (var item in _items)
+                yield return item;
+        }
+
+        public bool isEmpty
+        {
+            get
+            {
+                return _items.Count == 0;
+            }
+        }
+    }
+}
---
+++ Games/mv/mvserver/mvResponseActionItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvserver
+{
+    [Serializable]
+    internal class mvResponseActionItem : mvResponseItemBase
+    {
+        public override string msgType { get { return "a"; } }
+        private object[] _prms;
+        private ActionKind _actionKind;
+
+        public ActionKind ActionKind { get { return _actionKind; } }
+
+        public mvResponseActionItem(ActionKind actionKind, params object[] p)
+        {
+            _prms = p;
+            _actionKind = actionKind;
+        }
+
+        public object[] Items { get { return _prms.ToArray(); } }
+    }
+}
